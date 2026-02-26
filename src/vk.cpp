e3a8026d8e3bcd6b4b2bc855fa738e5f3c1cc7f2
ianpike <ianpike98@gmail.com>
2026-02-25 19:50:16 -0500

    Expand tests and harden TU

--- src/vk.cpp
+++ src/vk.cpp
@@ -1,3 +1,7 @@
 #include <vulkan/vulkan.hpp>
 
+#ifndef VULKAN_HPP_DISPATCH_LOADER_DYNAMIC
+#error VULKAN_HPP_DISPATCH_LOADER_DYNAMIC must be defined when building vk.cpp
+#endif
+
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
