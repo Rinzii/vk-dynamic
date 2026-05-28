75d44bab309a0cc0be472168a27e594ab2c5be6d
Ian Pike <Ianpike98@gmail.com>
2026-05-28 16:00:30 -0400

    Update sync script to generate Premake file

--- scripts/github_update_cmake.py
+++ scripts/github_update_cmake.py
@@ -52,3 +52,119 @@ if set_ver.strip() not in chk:
 if "VK_DYN_TAG" in chk:
   print("VK_DYN_TAG still present, expected it to be removed")
   sys.exit(1)
+
+premake = f"""VK_DYN_VER = "{vk_ver}"
+
+newoption {{
+    trigger = "vk-dynamic-no-prototypes",
+    value = "VALUE",
+    allowed = {{
+        {{ "on", "Enable" }},
+        {{ "off", "Disable" }}
+    }},
+    default = "on",
+    description = "Define VK_NO_PROTOTYPES for Vulkan headers"
+}}
+
+newoption {{
+    trigger = "vk-dynamic-beta-extensions",
+    value = "VALUE",
+    allowed = {{
+        {{ "on", "Enable" }},
+        {{ "off", "Disable" }}
+    }},
+    default = "on",
+    description = "Define VK_ENABLE_BETA_EXTENSIONS"
+}}
+
+newoption {{
+    trigger = "vk-dynamic-hpp-dynamic-dispatch",
+    value = "VALUE",
+    allowed = {{
+        {{ "on", "Enable" }},
+        {{ "off", "Disable" }}
+    }},
+    default = "on",
+    description = "Enable Vulkan-Hpp default dynamic dispatch storage and define"
+}}
+
+local function option_enabled(name)
+    local value = _OPTIONS[name]
+    return value == nil or value == "on"
+end
+
+workspace "vk-dynamic"
+    configurations {{ "Debug", "Release" }}
+    architecture "x86_64"
+    location "build/premake"
+
+    filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+    filter {{}}
+
+project "vk-dynamic"
+    kind "Utility"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{{cfg.buildcfg}}"
+    objdir "bin-int/%{{cfg.buildcfg}}/%{{prj.name}}"
+
+    files {{
+        "include/**.h",
+        "include/**.hpp"
+    }}
+
+    includedirs {{
+        "include"
+    }}
+
+    if option_enabled("vk-dynamic-no-prototypes") then
+        defines {{ "VK_NO_PROTOTYPES" }}
+    end
+
+    if option_enabled("vk-dynamic-beta-extensions") then
+        defines {{ "VK_ENABLE_BETA_EXTENSIONS" }}
+    end
+
+    if option_enabled("vk-dynamic-hpp-dynamic-dispatch") then
+        defines {{ "VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1" }}
+    end
+
+if option_enabled("vk-dynamic-hpp-dynamic-dispatch") then
+    project "vk-dynamic-hpp-dispatch"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++17"
+        targetdir "bin/%{{cfg.buildcfg}}"
+        objdir "bin-int/%{{cfg.buildcfg}}/%{{prj.name}}"
+
+        files {{
+            "src/vk.cpp"
+        }}
+
+        includedirs {{
+            "include"
+        }}
+
+        defines {{ "VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1" }}
+
+        if option_enabled("vk-dynamic-no-prototypes") then
+            defines {{ "VK_NO_PROTOTYPES" }}
+        end
+
+        if option_enabled("vk-dynamic-beta-extensions") then
+            defines {{ "VK_ENABLE_BETA_EXTENSIONS" }}
+        end
+end
+"""
+
+open("premake5.lua", "w", encoding="utf-8", newline="").write(premake)
+
+premake_chk = open("premake5.lua", "r", encoding="utf-8").read()
+if f'VK_DYN_VER = "{vk_ver}"' not in premake_chk:
+  print("Failed to set VK_DYN_VER in premake5.lua")
+  sys.exit(1)
